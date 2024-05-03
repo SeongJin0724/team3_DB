270ef862378af79b093f1af5ad6f18e76ff03d17
lsj <thls702464@gmail.com>
2024-05-03 11:16:12 +0900

    🐛 상세페이지수정

--- app.js
+++ app.js
@@ -267,10 +267,8 @@ app.get("/api/newin", async (req, res) => {
 
 // 상품 상세페이지
 app.get("/api/items/:itemKey", async (req, res) => {
-  let conn;
   try {
-    conn = await db.getConnection();
-    const rows = await conn.query("SELECT * FROM item WHERE itemKey = ?", [
+    const rows = await db.query("SELECT * FROM item WHERE itemKey = ?", [
       req.params.itemKey,
     ]);
     if (rows.length > 0) {
@@ -282,7 +280,7 @@ app.get("/api/items/:itemKey", async (req, res) => {
     console.error(err);
     res.status(500).send("서버 에러");
   } finally {
-    if (conn) conn.end();
+    if (db) db.end();
   }
 });
 
